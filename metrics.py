6a0a2427100d0c0b6a8b59eb7ea4994a95c7934d
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2023-12-01 22:35:44 +0800

    backup

--- metrics.py
+++ metrics.py
@@ -1,10 +1,8 @@
 import numpy as np
 
 def compute_rmse(predictions, actual):
-    # write code here
-    return np.sqrt((np.square((predictions - actual))).sum()/len(predictions))\
+    return np.sqrt((np.square((predictions - actual))).sum()/len(predictions))
 
 def compute_accuracy(predictions, actual):
-    # write code here
     return round(sum(p == a for p, a in zip(predictions, actual))/len(actual) * 100, 3)
 
